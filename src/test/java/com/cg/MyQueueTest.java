af57cd6561b1a0f3f878f0a45214bf5fa22e5a87
krove <krove@DESKTOP-Q5SED1R>
2020-10-12 01:30:32 +0530

    [Kashif]Created:Created Queue with Enqueu Method Internally calling Append method

---
+++ src/test/java/com/cg/MyQueueTest.java
@@ -0,0 +1,24 @@
+package com.cg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyQueueTest {
+	@Test
+	public void given3NumberWhenAddedToQueueShouldHaveFirstAddedNodeAtTop() {
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+
+		MyQueue myQueue = new MyQueue();
+		myQueue.enqueue(firstNode);
+		myQueue.enqueue(secondNode);
+		myQueue.enqueue(thirdNode);
+
+		AllNode top = myQueue.peek();
+		myQueue.printQueue();
+		assertEquals(firstNode, top);
+	}
+
+}
